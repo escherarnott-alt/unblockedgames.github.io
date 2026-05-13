9950f855bfa38ab11d5c7a7691e3ebde30c3f39b
escherarnott-alt <escher.arnott@icloud.com>
2026-05-13 11:13:17 +1000

    Add files via upload

---
+++ unblocked games website/script.js
@@ -0,0 +1,16 @@
+const search = document.getElementById("search");
+const cards = document.querySelectorAll(".game-card");
+
+search.addEventListener("input", () => {
+  const value = search.value.toLowerCase();
+
+  cards.forEach(card => {
+    const title = card.innerText.toLowerCase();
+
+    if (title.includes(value)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
